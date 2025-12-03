f08c7cee437f6bf17fdf97d7270b68f420c4eb2e
victorhugoRuralTech <victorhugocantarin911@gmail.com>
2025-12-03 20:14:16 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Criado com a Lasy",
-  description: "Projeto criado com Lasy AI",
+  title: "RuralTech",
+  description: "Estagio UMFG",
 };
 
 export default function RootLayout({
